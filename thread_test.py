c17c4eab5447483596c29333872ebc4da739aecf
Dennis Lutter <lad1337@gmail.com>
2011-05-18 00:47:19 +0200

    option to keep connection alive. auto reconnect. option to set a loger callback function. long time thread test

--- thread_test.py
+++ thread_test.py
@@ -37,11 +37,23 @@ for o,p in opts:
 # you only need to import the module
 import adba
 import threading
-import time
+from time import time,sleep,strftime,localtime
+
+def beep():
+    f=open('/dev/tty','w') 
+    f.write(chr(7)) 
+    f.close() 
+
+def getNowString():
+    return strftime("%Y-%m-%d %H:%M:%S", localtime(time())) 
+
+def log_function(data,logLvl="INFO"):
+    print(getNowString()+"-"+str(logLvl)+": "+str(data))
+logwrapper = lambda x :log_function("anidb: "+str(x),"DEBUG")
 
 # lets see the version
 print("version: "+str(adba.version))
-connection = adba.Connection(verbos=True)
+connection = adba.Connection(log=logwrapper,keepAlive=True)
 
 
 class ThreadLookUp(threading.Thread):
@@ -54,22 +66,58 @@ class ThreadLookUp(threading.Thread):
     
     def run(self):
         if not connection.authed():
-            print("authenticating in thread: "+self.getName())
+            log_function("authenticating in thread: "+self.getName())
             if(user and pw):
                 connection.auth(user, pw)
         else:
-            print("no need to authenticate some one else did it")
+            log_function("no need to authenticate some one else did it")
         
         if(self.animeName != ""):
             anime = adba.Anime(connection,name=self.animeName,paramsA=['aid'])
             anime.load_data()
-            print("the id to "+self.animeName+" is "+str(anime.aid))
+            log_function("the id to "+self.animeName+" is "+str(anime.aid))
         else:
-            print("not looking up anything you gave me no anime name");
+            log_function("not looking up anything you gave me no anime name");
 
 
 
 ThreadLookUp("One Piece").start()
+#ThreadLookUp("Bleach").start()
+log_function("sleeping 4 min...")
+sleep((60*4)+30)
+connection.logout() 
+connection.lastAuth = time()-1801
+connection._keep_alive()
+
+
+log_function("sleeping 10 min...")
+sleep(60*10)
+beep()
+log_function("sleeping another 10 min...")
+sleep(60*10)
+
+beep()
+beep()
+log_function("checking connection after 20 min")
+log_function("connection status: "+str(connection.authed()))
+
+log_function("sleeping another 10 min...")
+sleep(60*10)
+
+beep()
+beep()
+beep()
+log_function("checking connection after 30 min")
+log_function("connection status: "+str(connection.authed()))
+log_function("sleeping another 10 min...")
+sleep(60*10)
+
+beep()
+beep()
+beep()
+beep()
+print(getNowString()+": checking connection after 40 min")
+print(getNowString()+": connection status: "+str(connection.authed()))
+
+print("checking connection with a real request...")
 ThreadLookUp("Bleach").start()
-
-
